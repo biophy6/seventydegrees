3a7d0cccf2ce0091242ec4474feae6f5874ec2f3
Omar Bakht <biophy6@yahoo.com>
2013-03-14 11:40:36 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seventydegrees::Application.config.secret_token = 'cc54c511fbc537ecb9e1ea2213bfb2a1f9e0e31b34e750bebcaf453a689cc77fd52a254beebb8a37f1559e0439dc5ba8288c88ffe4416dcfe634cd511be26d09'
