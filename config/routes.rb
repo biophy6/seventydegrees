25edfd834285b78690961c40e8989f1b34dcc034
Omar Bakht <biophy6@yahoo.com>
2013-03-19 20:21:39 -0400

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Seventydegrees::Application.routes.draw do
+  
+  get 'about' => 'pages#about'
+  get 'faq' => 'pages#faq'
+  get 'contact' => 'pages#contact'
+  root :to => 'pages#home'
+  
+  
+  
+  #get "pages/home"
+  #get "pages/about"
+  #get "pages/faq"
+  #get "pages/contact"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
